9bac4d75162a612f28a2f2ee6e6f42c7bc57f9b8
xjwan <wxj@juzhi.cn>
2014-04-29 19:15:35 +0800

    [xjwan & gongsuo] resolve velocity character encoding.

--- src/main/java/com/juzhi/sale/controller/TestController.java
+++ src/main/java/com/juzhi/sale/controller/TestController.java
@@ -8,9 +8,11 @@ import com.juzhi.sale.dao.HelloDaoImpl;
 import com.juzhi.sale.entity.Hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,14 +28,14 @@ public class TestController {
     @Autowired
     private HelloDaoImpl helloDaoImpl;
 
-    @RequestMapping(value = "/test",method = RequestMethod.GET)
-   // @ResponseBody 直接返回string
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    // @ResponseBody 直接返回string
     public String testHello(ModelMap model) {
-       model.addAttribute("message","helloworld");
+        model.addAttribute("message", "helloworld");
         return "hello";
     }
 
-    @RequestMapping(value = "/vt",method = RequestMethod.GET)
+    @RequestMapping(value = "/vt", method = RequestMethod.GET)
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
         Map map = new HashMap();
@@ -47,13 +49,26 @@ public class TestController {
         return new ModelAndView("index", map);
     }
 
-    @RequestMapping(value = "/save",method = RequestMethod.GET)
-    public String saveHello(){
+    @RequestMapping(value = "/save", method = RequestMethod.GET)
+    public String saveHello() {
         Hello hello = new Hello();
         hello.setId(1);
         hello.setName("hello");
         helloDaoImpl.insert(hello);
 
         return "success";
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public ModelAndView selectByName(String name, Model model) {
+        name = "hello";
+        List list = helloDaoImpl.findByName(name);
+        model.addAttribute("test", list);
+        return new ModelAndView("success");
+    }
+
+    @RequestMapping("index.vm")
+    public ModelAndView test1() {
+        return new ModelAndView("pages/widgets");
     }
 }
