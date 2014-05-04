deec8d3f4665aa510c9385276281d2ded838b0aa
gschen <gongsuochen@163.com>
2014-05-04 09:12:54 +0800

    [gschen] add ip filter.

--- src/main/java/com/juzhi/sale/filter/IpFilter.java
+++ src/main/java/com/juzhi/sale/filter/IpFilter.java
@@ -1,6 +1,9 @@
 package com.juzhi.sale.filter;
 
+import org.apache.log4j.Logger;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -8,6 +11,9 @@ import java.io.IOException;
  * Created by gschen on 5/3/14.
  */
 public class IpFilter implements Filter {
+
+
+    static Logger logger = Logger.getLogger(Filter.class);
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -18,9 +24,9 @@ public class IpFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-        System.out.println("----------->" + request.getRemoteAddr());
-
         String ip = request.getRemoteAddr();
+
+        logger.info("--> Incoming Request IP: " + request.getRemoteAddr());
 
         //TODO: forbidden some ip address
 //        if(ip.equals("192.168.1.4")){
